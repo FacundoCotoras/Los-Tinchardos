b50422a4aade8506878d5178886f2474330a7830
LittleMonkey47 <benjamin.sala1.2.et32@gmail.com>
2026-05-28 12:28:15 -0300

    Delete Joins Dificiles/1)/consultas/3)/consultas directory

--- Joins Dificiles/1)/consultas/3)/consultas/3)/3).sql
+++
@@ -1,9 +0,0 @@
-select categorias.nombre as categoria, sum(detalleventa.Cantidad * detalleventa.PrecioUnidad) as recaudaciontotal
-from categorias
-inner join productos
-on categorias.IDCategoria = productos.IDCategoria
-inner join detalleventa
-on productos.IDProducto = detalleventa.IDProducto
-where productos.Precio < 100
-group by categorias.IDCategoria, categorias.nombre 
-having sum(detalleventa.Cantidad * detalleventa.PrecioUnidad) < 5000;
